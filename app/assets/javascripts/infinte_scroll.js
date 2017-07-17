470d7a0ee3a47db79dbe15aa8be3b16a6a68964b
Cameron Webster <cmwebster90@googlemail.com>
2017-07-17 12:33:48 +0100

    reverted to pagination

---
+++ app/assets/javascripts/infinte_scroll.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+$(window).scroll(function(){
+  if ($(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+      // $.getScript($('.pagination .next_page').attr('href'));
+              // var grid = document.querySelector('#grid');
+              // var item = document.createElement("div");
+      // $.getScript($('.pagination .next_page').attr('href'));
+
+
+        //   console.log(item);
+        // salvattore.appendElements(grid, [item]);
+
+
+  }
+})
+
+
+})
